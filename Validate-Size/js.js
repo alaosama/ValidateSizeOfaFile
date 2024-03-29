2ef9e151e48c54eb467bc1d9a4480058c0915b2c
alaosama <alahyaneosama1@gmail.com>
2024-03-29 17:34:55 +0100

    Validate The Size Of a File

---
+++ Validate-Size/js.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('input[type="file"');
+const userInfo = document.getElementById('infos');
+
+// Validate Size
+
+function validatesize() {
+    const file = input.files[0];
+    if (!file) {
+        const error = new Error ("No File To Selected");
+        userInfo.textContent = error.message;
+        return error;
+    }
+
+    const limit = 2000;
+    const size = file.size / 1024;
+    if (size > limit) {
+        const error = new Error(`File :${(size / 1000).toFixed(2)} MB`);
+        userInfo.textContent = err.message;
+        return error;
+    } else {
+        userInfo.textContent = "Great";
+        return false;
+    }
+
+}
